db3c9cb2cdcb62595062e54765ad56a12e825a01
Ubuntu <ubuntu@ip-172-31-30-81.us-east-2.compute.internal>
2018-07-20 22:44:29 +0000

    added table and hyperlink

--- app.py
+++ app.py
@@ -6,7 +6,29 @@ def hello_world():
 	return 'Hello, world\n\n'
 @app.route('/brian')
 def hello_brian():
-	return 'Hello.... brian...\n\n\n\n\n\n'
+	return """
+	<b>Hello brian</b>
+	<a href="table">back to table</a>
+	"""
+
+@app.route('/table')
+def create_table():
+	return """
+	<table>
+	<tr>
+		<th>Firstname</th>
+		<th>Lastname</th>
+		<th>Age</th>
+	</tr>
+	<tr>
+		<td>Jill</td>
+		<td>Stein</td>
+		<td>506</td>
+	</tr>
+	</table>
+	<a href="brian">hello</a>
+	"""
+
 
 if __name__ == "__main__":
 	app.run(host="ec2-18-217-6-113.us-east-2.compute.amazonaws.com", port=80)
